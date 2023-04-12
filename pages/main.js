bfc25030e6adc3fcf7cdb9b3a9a0dc3346dba0bf
kaho <cg.yaruzo.material.daisuki@gmail.com>
2023-04-13 01:04:12 +0900

    just changed

--- pages/main.js
+++ pages/main.js
@@ -5,7 +5,7 @@ export default function Main() {
     <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
       <Link href="/myPage">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          My Page
+          My Page aaaaaa
         </button>
       </Link>
       <Link href="/sendEvaluation">
